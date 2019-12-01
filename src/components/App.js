66a4a5f2d8ddbd5a0f2bb94ab752125aae86a9a9
Yiyi <414956851@qq.com>
2019-12-01 15:03:24 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import axios from "axios";
+import SearchBar from "./SearchBar";
+
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID 481745d4d64150145b4df6ac5dd865953e76334ad730e75b49a44443e0df0efb"
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
+}
+
+export default App;
